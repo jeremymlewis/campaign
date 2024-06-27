f09bceaa55002d5c9f6e9337ab8331178dd09923
jeremymlewis <jeremylewis086@gmail.com>
2024-06-27 12:02:00 +1000

    Update backend endpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,8 @@ import { AppComponent } from './app.component';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 //import { InAppPurchase2 } from '@awesome-cordova-plugins/in-app-purchase-2/ngx';
-
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+//https://54.189.223.171:3000
+const config: SocketIoConfig = { url: 'https://windriveradmin.info:3000', options: {} };
 
 @NgModule({
   declarations: [AppComponent],
